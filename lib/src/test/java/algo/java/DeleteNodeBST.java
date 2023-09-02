bad5a9be492662dc319267d15340498a977b3cfd
Arseni Kavalchuk <ars.kov@gmail.com>
2023-09-02 20:53:12 +0100

    Formatting

--- lib/src/test/java/algo/java/DeleteNodeBST.java
+++ lib/src/test/java/algo/java/DeleteNodeBST.java
@@ -6,23 +6,26 @@ import org.junit.Test;
 import algo.java.TreeNodeUtils.TreeNode;
 
 public class DeleteNodeBST {
-    
+
     class Solution {
 
         private int successor(TreeNode root) {
             var next = root.right;
-            while (next.left != null) next = next.left;
+            while (next.left != null)
+                next = next.left;
             return next.val;
         }
-    
+
         private int predecessor(TreeNode root) {
             var next = root.left;
-            while (next.right != null) next = next.right;
+            while (next.right != null)
+                next = next.right;
             return next.val;
         }
-    
+
         public TreeNode deleteNode(TreeNode root, int key) {
-            if (root == null) return null;
+            if (root == null)
+                return null;
             if (key > root.val) {
                 root.right = deleteNode(root.right, key);
             } else if (key < root.val) {
@@ -46,12 +49,12 @@ public class DeleteNodeBST {
     public void deleteNodeBSTTest() {
         var solution = new Solution();
 
-        var test1 = TreeNode.of(new Integer[] {5,3,6,2,4,null,7});
-        var expected1 = TreeNode.of(new Integer[] {5,4,6,2,null,null,7});
+        var test1 = TreeNode.of(new Integer[] { 5, 3, 6, 2, 4, null, 7 });
+        var expected1 = TreeNode.of(new Integer[] { 5, 4, 6, 2, null, null, 7 });
         Assert.assertEquals(null, expected1, solution.deleteNode(test1, 3));
 
-        var test2 = TreeNode.of(new Integer[] {5,3,6,2,4,null,7});
-        var expected2 = TreeNode.of(new Integer[] {5,3,6,2,4,null,7});
+        var test2 = TreeNode.of(new Integer[] { 5, 3, 6, 2, 4, null, 7 });
+        var expected2 = TreeNode.of(new Integer[] { 5, 3, 6, 2, 4, null, 7 });
         Assert.assertEquals(null, expected2, solution.deleteNode(test2, 0));
 
         var test3 = TreeNode.of(new Integer[0]);
